9dd43d64faebda2a45b00fdb2bdf94cf6d5e987a
Kevin-patzan <101298106+Kevin-patzan@users.noreply.github.com>
2022-10-26 11:51:58 -0600

    Add files via upload

---
+++ Madrid-PatzanTaller1/JS/js.js
@@ -0,0 +1,8 @@
+function getRandomInt(max){
+    return Math.floor(Math.random() * max)
+
+    console.log(getRandomInt(4));
+    console.log(getRandomInt(3));
+    console.log(getRandomInt(2));
+
+}
